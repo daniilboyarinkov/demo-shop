654d96438cddf519d9d5cf6e7a7ec2c358c746a7
daniilboyarinkov <daniilboyarinkov@gmail.com>
2023-01-19 19:32:56 +0500

    hero-bg-image-fixed

--- src/components/Sections/HeroMain.tsx
+++ src/components/Sections/HeroMain.tsx
@@ -7,7 +7,7 @@ import { EndPoints } from "../../const/endpoints"
 
 // export interface IHeroMainProps {}
 
-const bgImg: string =
+const bgImg =
   "https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
 
 export function HeroMain() {
